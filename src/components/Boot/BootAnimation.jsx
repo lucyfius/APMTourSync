9e74dc924f89b04914b42e6ae33941cf076225ac
lili <me@lota.dev>
2025-01-03 14:18:54 -0800

    2.5 > 5% increase for better odds

--- src/components/Boot/BootAnimation.jsx
+++ src/components/Boot/BootAnimation.jsx
@@ -81,7 +81,7 @@ export default function BootAnimation({ onComplete }) {
   };
 
   useEffect(() => {
-    if (Math.random() < 0.025) {
+    if (Math.random() < 0.050) {
       const easterEggs = [
         {
           logo: getEasterEggPath('easter1.png'),
